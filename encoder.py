bd22a4e42270539cf58e7a18f2ff5d3a87ba1e0c
Akela Craviotto-Velasco <akelacv@gmail.com>
2023-12-14 17:47:39 -0800

    added doc strings to encoder

--- encoder.py
+++ encoder.py
@@ -1,13 +1,24 @@
 from pyb import Timer
 class Encoder:
-    
+    """The Encoder class interfaces with the encoder that is encased within the motor.
+
+    """
+
     def __init__(self,ENC_tim,CH_A_PIN,CH_B_PIN):
+          """__init__ method for the encoder.
+
+        Args:
+            ENC_tim (str): The timer channel of the encoder.
+            CH_A_PIN (str): The pin object of the encoder for motor A
+            CH_B_PIN (str): The pin object of the encoder for motor B
+        """
         self.ench1 = ENC_tim.channel(1, pin=CH_A_PIN, mode=Timer.ENC_AB)
         self.ench2 = ENC_tim.channel(2, pin=CH_B_PIN, mode=Timer.ENC_AB)
         self.Timer = ENC_tim       
         pass
     
     def update(self):       
+         """Updates the current position of the encoder"""
         self.count_new = self.Timer.counter()
         self.delta = self.count_new-self.count_old
         
@@ -40,7 +51,7 @@ class Encoder:
         return self.delta
     
     def zero(self):
-        
+         """str: Zeros the encoder"""
         self.pos = 0
         self.count_old = 0
         self.count_new = 0
